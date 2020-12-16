9f4f1b2b35a952e92df161df949dc92ff5647fda
nazmul53p <nazmul2018s@gmail.com>
2020-12-16 23:23:47 +0600

    add button

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
-let low = 1;
-let high = 10;
-let correcr_ans = Math.floor((Math.random() * high) + low);
+document.getElementById("game").addEventListener("click",startGame)
 
-let count = 0
-for (let i = 1; i <= 3; i++) {
-    let input = parseInt(prompt("Guessing the number between 1 to 10:"));
-     if (input === correcr_ans) {
-        alert("You Win!")
-        break;
-    }else if (input < correcr_ans) {
-        alert("Correct answer is greater!")
-    } else if (input > correcr_ans) {
-        alert("Correct answer is smaller!")
+function startGame() {
+    let low = 1;
+    let high = 10;
+    let correcr_ans = Math.floor((Math.random() * high) + low);
+
+    let count = 0
+    for (let i = 1; i <= 3; i++) {
+        let input = parseInt(prompt("Guessing the number between 1 to 10:"));
+        if (input === correcr_ans) {
+            alert("You Win!")
+            break;
+        }else if (input < correcr_ans) {
+            alert("Correct answer is greater!")
+        } else if (input > correcr_ans) {
+            alert("Correct answer is smaller!")
+        }
+        count +=1
     }
-     count +=1
-}
 
-if (count===3) {
-    alert("You Loss!")
+    if (count===3) {
+        alert("You Loss!")
+    }
+
 }
